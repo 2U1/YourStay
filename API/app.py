953756c90e17f34008d8b62c9f5430d05ea823ab
2U1 <kirito01@naver.com>
2021-05-20 16:44:44 +0900

    recommend added

--- API/app.py
+++ API/app.py
@@ -3,9 +3,11 @@ import bcrypt
 from dns import message
 from flask import Flask, request, Response, jsonify, redirect, url_for
 import json
+from flask_jwt_extended.utils import get_jwt_identity
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from operator import itemgetter
 
 
 app = Flask(__name__)
@@ -38,7 +40,7 @@ def register():
 
     else:
         hased = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        user_data = {'user_id':user_id, 'password':hased, 'user_name':user_name, 'cleanliness':int(clean), 'convinience':int(conv), 'kindess':int(kind), 'position':int(position) }
+        user_data = {'user_id':user_id, 'password':hased, 'user_name':user_name, 'cleanliness':int(clean), 'convenience':int(conv), 'kindness':int(kind), 'position':int(position) }
         db.user.insert_one(user_data)
 
         return jsonify(message='User Registered')
@@ -74,7 +76,7 @@ def login():
 
 
 @app.route('/api/v1/hotels', methods=['GET'])
-@jwt_required
+@jwt_required()
 def get_hotel():
     hotels = db.hotel
     output = []
@@ -85,9 +87,35 @@ def get_hotel():
 
 
 
-# @app.route('/api/v1/hotels/<int:hotelid>')
-# @jwt_required
-# def get_info(hotelid):
+@app.route('/api/v1/hotels/recommended', methods=['GET'])
+@jwt_required()
+def recommend_hotel():
+    hotels = db.hotel
+    users = db.user
+    
+    current_user = get_jwt_identity()
+    user_info = users.find_one({'user_id':current_user})
+    user_scores = {'cleanliness':user_info['cleanliness']*0.1, 'position':user_info['position']*0.1, 'convenience': user_info['convenience']*0.1, 'kindness':user_info['kindness']*0.1}
+
+    hotel_infos = list(hotels.find())
+
+    weighted_scores = []
+
+    for h in hotel_infos:
+        weighted = {}
+        weighted['name'] = h['hotelName']
+        weighted['score'] = (user_scores['cleanliness']*h['cleanliness'] + user_scores['position']*h['position'] + user_scores['convenience']*h['convenience'] + user_scores['kindness']*h['kindness']) / 4
+        weighted_scores.append(weighted)
+
+    
+    recommended = sorted(weighted_scores, key = itemgetter('score'), reverse=True)[0:5]
+
+    return jsonify({'result':recommended})
+    
+
+# @app.route('/api/v1/hotels/reviews/<int:hotelid>', methods=['GET'])
+# @jwt_required()
+# def get_review(hotelid):
 #     tokenized = db.tokenized_review
 #     output = []
 #     info = tokenized.find({'hotel_id':hotelid})
