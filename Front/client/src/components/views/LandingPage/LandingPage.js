f699c5ed648aacc39d81d898ed00a8dfe0df061d
yuls12 <yuluri129@gmail.com>
2021-05-20 18:43:59 +0900

    Create LandingPage.js

---
+++ Front/client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+import logo from '../../../logo.png';
+import HotelPage from '../HotelPage/HotelPage'
+
+function LandingPage() {
+    return (
+        <>
+            <div className="app">
+                <HotelPage />
+            </div>
+        </>
+    )
+}
+
+export default LandingPage
