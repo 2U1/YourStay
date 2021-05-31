4f4709fa933d9e8bf250f7e9c4f2e392cde2a548
Yu-seok Jeong <59636424+j961224@users.noreply.github.com>
2021-05-31 23:31:38 +0900

    Delete extensions.py

--- Api/pybo/api_v2/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_jwt_extended import JWTManager
-from flask_cors import CORS
-from flask_bcrypt import Bcrypt, generate_password_hash, check_password_hash
-
-
-jwt = JWTManager()
-cors = CORS(resources={r"/": {"origins": "http://localhost:5000"}})
-bcrpyt = Bcrypt()
